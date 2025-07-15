5c74a9f2b2d14288cf2f6cc748c8391ce90cd8dc
joleru <jorgeleonardo30@gmail.com>
2025-07-14 23:00:24 -0500

    subida git

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import '../App.css'
+function Main(){
+
+    return(
+        <>
+            <header>
+                <nav className="navbar">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>  
+                    <a href="/contact">Contact</a>
+                    <a href="/blog">Blog</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Main;
